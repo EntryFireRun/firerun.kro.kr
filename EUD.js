019f4e1a1f1829f6ac9e2d18a6573c3ba2b45414
FireRun <entryfirerun7012@outlook.com>
2024-07-14 13:09:33 +0900

    Update EUD.js

--- EUD.js
+++ EUD.js
@@ -1,3 +1,7 @@
+if(localStorage["newAlertTime"] == undefined) {
+  localStorage["newAlertTime"] = 0
+}
+
 if((document.querySelector('.removePromotionVersion').innerText < 2)&&(localStorage["newAlertTime"] <= new Date().getTime())) {
   if(confirm('새 버전의 업데이트가 있습니다 설치할까요?\n취소를 하시면 1일 뒤에 다시 안내됩니다')) {
     alert('설치는 알아서 하세요ㅋ')
